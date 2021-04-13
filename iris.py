5d704f3a16d5369530b7dc59fc4924bfd7fa1df6
Emil Haugstvedt <emiljha@stud.ntnu.no>
2021-04-13 16:05:10 +0200

    Tatt greit med inspo, men ligner på noe

--- iris.py
+++ iris.py
@@ -1,6 +1,16 @@
 from sklearn import datasets
+import numpy as np
 
+## Defining constants ##
+traingStart = 0
+traingStop = 30
+testingStart = 30
+testingStop = 50 
 
+## Parameters for linear classifier ##
+alpha = 0.01
+N_iter = 5000 # Number of iterations
+D = 4 #Number of features
 
 
 def extractData(irisData):
@@ -9,15 +19,70 @@ def extractData(irisData):
     class3 = irisData[100:150]
     return class1, class2, class3
 
-def testingData(irisClass):
-    return irisClass[30:50]
+def trainingData(irisClass, traingStart, traingStop):
+    return irisClass[traingStart:traingStop]
 
-def traingData(irisClass):
-    return irisClass[0:30]
+def testingData(irisClass, testingStart, testingStop):
+    return irisClass[testingStart:testingStop]
+
+def linearClassifier(classes, N_iter, N_training, N_testing):
+    features = [0,1,2,3]
+
+    C = len(classes) # Number of classes
+    F = len(features) # Number of features
+
+    W = np.zeros((C, F))
+    w_0 = np.zeros((C, 1))
+
+    W = np.concatenate((W, w_0), axis=1)
+
+    # Extracting training data #
+    training1 = trainingData(classes[0], 0, N_training)
+    training2 = trainingData(classes[1], 0, N_training)
+    training3 = trainingData(classes[2], 0, N_training)
+    training = [training1, training2, training3]
+
+
+    # Extracting testing data #
+    testing1 = testingData(classes[0], N_training, N_training + N_testing)
+    testing2 = testingData(classes[1], N_training, N_training + N_testing)
+    testing3 = testingData(classes[2], N_training, N_training + N_testing)
+
+    # Creating targets #
+    target_k1 = np.array([[1],[0],[0]]) #class1
+    target_k2 = np.array([[0],[1],[0]]) #class2
+    target_k3 = np.array([[0],[0],[1]]) #class3
+    target_k  = [target_k1, target_k2, target_k3]
+
+    for _ in range(N_iter):
+        W_prev = W
+        grad_MSE = np.zeros((C,F))
+        for k in range(N_training):
+            for (xk, tk) in zip(training, target_k):
+                xk = np.append(xk[k], 1)
+                xk = xk.reshape(F+1, 1)
+                zk = W@xk
+                gk = sigmoid(zk)
+                temp = np.multiply(gk-tk, gk)
+                temp = np.multiply(temp, np.ones((C,1))-gk)
+                grad_MSE += temp@xk.T
+        W = W_prev - alpha*grad_MSE
+    return W
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+
 
 
 
 if __name__ == "__main__":
     irisData = datasets.load_iris()['data']
     class1, class2, class3 = extractData(irisData)
-    print(class1, class2)
+    classes = [class1, class2, class3]
+    W = linearClassifier(classes, 100, 30, 20)
+    print(W)
+
+
